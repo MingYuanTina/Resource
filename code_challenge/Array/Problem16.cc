08ae4e4d289d826ec6191a719c13fcdf1485fc85
MingYuanTina <bjteana@gmail.com>
2017-07-26 21:46:06 -0400

    array p16

--- code_challenge/Array/Problem16.cc
+++ code_challenge/Array/Problem16.cc
@@ -5,12 +5,6 @@
 	// You may assume that nums1 has enough space (size that is greater or equal to m + n) 
 	// to hold additional elements from nums2. The number of elements initialized in nums1 and nums2 are m and n respectively.
 
-// Constraint
-	// Not specified
-
-// Test Cases
-
-// General Strategy
 
 // Solution
 class Solution {
